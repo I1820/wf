fad56432fbecd910cbbc741f06bd1080fb3528ce
Parham Alvani <parham.alvani@gmail.com>
2018-11-12 11:23:07 +0330

    Adds darksky api endpoints.

---
+++ actions/darksky.go
@@ -0,0 +1,47 @@
+/*
+ *
+ * In The Name of God
+ *
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 12-11-2018
+ * |
+ * | File Name:     darksky.go
+ * +===============================================
+ */
+
+package actions
+
+import (
+	"net/http"
+
+	"github.com/I1820/wf/darksky"
+	"github.com/gobuffalo/buffalo"
+)
+
+// darksky request payload
+type darkskyReq struct {
+	Latitude  float64 `json:"lat" validate:"required"`
+	Longitude float64 `json:"long" validate:"required"`
+}
+
+// DarkskyHandler uses darksky API to forecast weather in give geolocation.
+func DarkskyHandler(c buffalo.Context) error {
+	var rq darkskyReq
+
+	if err := c.Bind(&rq); err != nil {
+		return c.Error(http.StatusBadRequest, err)
+	}
+
+	if err := validate.Struct(rq); err != nil {
+		return c.Error(http.StatusBadRequest, err)
+	}
+
+	ws, err := darksky.ForecastRequest(rq.Latitude, rq.Longitude)
+	if err != nil {
+		return c.Error(http.StatusInternalServerError, err)
+	}
+
+	return c.Render(http.StatusOK, r.JSON(ws))
+}
